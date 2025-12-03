c489814552f68f91e18cfb13f7ef3c4f8182084e
bmalvo <boydmalvo@gmail.com>
2025-12-03 22:04:30 +0100

    two way binding

--- VueJS-CrashCourse/src/App.vue
+++ VueJS-CrashCourse/src/App.vue
@@ -2,7 +2,7 @@
 import { ref, onMounted } from 'vue'
 
 const count = ref(0);
-const message = 'Patrick';
+const message = defineModel();
 const number = 10;
 const isActive = true;
 const url = 'https://edwindiaz.com'
@@ -21,14 +21,21 @@ onMounted(() => {
 <template>
 
 
-  <div id="app">   
-    <button @click="increment">
+  <div class="container" id="app">   
+    <button class="btn btn-primary" @click="increment">
       Count is: {{ count }}
     </button> 
     <h2>{{message}}</h2>
     <h2>{{number * 2}}</h2>
     <h2>{{isActive ? 'is active' : 'is not active'}}</h2>
     <h2><a :href=url >Link</a></h2>
+    <h1>Two way binding</h1>
+    <form action="">
+      <div class="col-sm-6">
+        <input v-model="message" type="text" class="form-control">
+        <p>message: {{message}}</p>
+      </div>
+    </form>
   </div>
 
 </template>
