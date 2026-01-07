3f73b5fdb87287e325836739bee2c9375fe6218b
bmalvo <boydmalvo@gmail.com>
2026-01-07 21:57:53 +0100

    refactoring Welcome-component

--- VueJS-CrashCourse/src/Welcome-component.vue
+++ VueJS-CrashCourse/src/Welcome-component.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 
 import {ref} from 'vue'
-</script>
-<script lang="ts">
+const messageText = ref('Welcome to my page!');
 
-    const messageText = ref('Welcome to my page!');
 </script>
 
 <template>
+
     <h2>{{ messageText }}</h2>
+
 </template>
 
 <style>
