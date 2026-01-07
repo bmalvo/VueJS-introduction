35b9d08ef6d4eccefb4e6b82bb434f21b539ce37
bmalvo <boydmalvo@gmail.com>
2026-01-07 21:58:41 +0100

    added Adding-button

---
+++ VueJS-CrashCourse/src/Adding-button.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+    count?: number
+    increment?: (payload: PointerEvent) => void
+}>()
+</script>
+<!-- <script setup lang="ts">
+</script> -->
+
+<template>
+ <button class="btn btn-primary" @click="props.increment">
+      Count is: {{ props.count }}
+    </button> 
+</template>
+
+<style>
+
+</style>
