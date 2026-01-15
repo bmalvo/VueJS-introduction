07e7440f6a6b21181cbe566b35179088c384aae5
bmalvo <boydmalvo@gmail.com>
2026-01-15 22:03:44 +0100

    add component Swich-button.vue

---
+++ VueJS-CrashCourse/src/Swich-button.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps<{
+    action?: (payload: PointerEvent) => void
+}>()
+
+// const action = () => {
+
+//     return !props.value
+// }
+</script>
+
+<template>
+ <button class="btn btn-primary" @click="props.action">
+      toggle
+    </button> 
+</template>
+
+<style>
+
+</style>
