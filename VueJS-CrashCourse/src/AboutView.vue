45594258b5e6b487c1a8bf6d36fa2e4046921ff3
bmalvo <boydmalvo@gmail.com>
2026-03-12 20:29:31 +0100

    add aboutview

---
+++ VueJS-CrashCourse/src/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="about">
+        <h1>AboutView</h1>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'AboutView'
+}
+</script>
+
+<style scoped>
+.about {
+    padding: 20px;
+}
+
+h1 {
+    font-size: 2rem;
+    color: #333;
+}
+</style>
