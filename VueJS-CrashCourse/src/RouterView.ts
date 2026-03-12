ed12387da25eb0806e324f857ca2750bac64c909
bmalvo <boydmalvo@gmail.com>
2026-03-12 20:28:53 +0100

    add routerview

---
+++ VueJS-CrashCourse/src/RouterView.ts
@@ -0,0 +1,16 @@
+// <script lang=ts>
+import { createMemoryHistory, createRouter } from 'vue-router'
+
+import HomeView from './HomeView.vue'
+import AboutView from './AboutView.vue'
+
+const routes = [
+  { path: '/', component: HomeView },
+  { path: '/about', component: AboutView },
+]
+
+export const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+// </script>
