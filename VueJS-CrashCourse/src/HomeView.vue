d370a2ce63131e5ef1bf3aa0f877d3346c7f7730
bmalvo <boydmalvo@gmail.com>
2026-03-12 20:30:54 +0100

    add homeview

---
+++ VueJS-CrashCourse/src/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="home">
+        <h1>Home View</h1>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'HomeView'
+}
+</script>
+
+<style scoped>
+.home {
+    padding: 20px;
+}
+
+h1 {
+    font-size: 2rem;
+    color: #333;
+}
+</style>
